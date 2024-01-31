d854734ec933721129d9770ecd6414b7649ca1dd
X7k-Lowe <143049442+X7k-Lowe@users.noreply.github.com>
2024-01-31 23:31:09 +0900

    スコアボードレコードImage修正・レーザーサイトNullポ修正

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -67,6 +67,7 @@ public class GameManager : MonoBehaviourPunCallbacks, IOnEventCallback
 
     public bool isDead { get; set; } = false;
     public Material blackOutMaterial;
+    public LineRenderer laserSight;
     private void Awake()
     {
         if (!isStart)
